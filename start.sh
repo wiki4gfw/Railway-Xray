200c16afaee55f4f8d0f64490e3417630054c18f
wiki4gfw <72778562+wiki4gfw@users.noreply.github.com>
2021-11-08 10:38:10 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=24b4b1e1-7a29-45f1-858c-242cf51b5bdb
+AUUID=d7662dde-1d41-4c35-888b-033f1764b643
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
